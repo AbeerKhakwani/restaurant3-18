45797eae14e78d58bd6514877e11e3a6cf3aed6c
abeer khakwani & Conor Baumgart & erica cha <student@epicodus.com>
2015-03-18 16:49:40 -0700

    Add some stuff

--- app/app.php
+++ app/app.php
@@ -15,28 +15,64 @@
     ));
 
     $app->get("/", function() use ($app) {
-    return $app['twig']->render('index.twig', array('cuisine' => Cuisine::getAll()));
+    return $app['twig']->render('index.twig', array('cuisines' => Cuisine::getAll()));
 });
 
-
-    $app->post("/cuisines", function() use ($app) {
+    $app->post("/", function() use ($app) {
         $new_cuisine=new Cuisine($_POST['type']);
         $new_cuisine->save();
-    return $app['twig']->render('index.twig', array('cuisine' => Cuisine::getAll()));
+        return $app['twig']->render('index.twig', array('cuisines' => Cuisine::getAll()));
     });
-
-
 
     $app->get("/cuisine/{id}", function($id) use ($app) {
         $cuisine = Cuisine::find($id);
-        return $app['twig']->render('cuisine.twig', array('cuisine' => $cuisine));
+        return $app['twig']->render('cuisine.twig', array('cuisines' => $cuisine));
     });
 
-    $app->post("/delete_cuisines", function() use ($app) {
-        Cuisine::deleteAll();
-        return $app['twig']->render('index.twig', array('cuisine' => Cuisine::getAll()));
-    });
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //
+    // $app->get("/restaurants", function() use ($app) {
+    //
+    // return $app['twig']->render('restaurant.twig', array('restaurants' => Restaurant::getAll()));
+    // });
+    //
+
+    // $app->post("/delete_cuisines", function() use ($app) {
+    //     Cuisine::deleteAll();
+    //     return $app['twig']->render('index.twig', array('cuisine' => Cuisine::getAll()));
+    // });
+    //
+    // $app->post("/restaurants", function() use ($app) {
+    //    $name= $_POST['name'];
+    //    $address= $_POST['address'];
+    //    $cuisine_id= $_POST['cuisine_id'];
+    //    $new_res=new Restaurant ($id = null, $name,$address,$cuisine_id );
+    //    $new_res->save();
+    //
+    //     Restaurant::getAll();
+    //     return $app['twig']->render('cuisine.twig', array('cuisines' => Cuisine::getAll()));
+    // });
 
 return $app;
 
